d2a15199ce3b575c27aa3f8b6b3578a9c2e8bb83
Markus Wüstenberg <markus@maragu.dk>
2025-03-06 20:57:45 +0100

    Rename env var FILE to MODEL

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 .PHONY: build
-
 build:
 	docker build -t llama-server .
 
+.PHONY: down
+down:
+	docker compose down
+
+.PHONY: run
 run:
 	docker run -it --rm llama-server
 
+.PHONY: up
 up:
 	docker compose up -d
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE=${FILE:-"Llama-3.2-3B-Instruct-Q4_K_M.gguf"}
-echo "Downloading $FILE into /models/"
-curl -f -L -C - -o /models/$FILE https://assets.maragu.dev/llm/$FILE
-/app/llama-server -m /models/$FILE
+MODEL=${MODEL:-"Llama-3.2-3B-Instruct-Q4_K_M.gguf"}
+echo "Downloading $MODEL into /models/"
+curl -f -L -C - -o /models/$MODEL https://assets.maragu.dev/llm/$MODEL
+/app/llama-server -m /models/$MODEL
